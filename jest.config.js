c35c6a731218ad501fb2e06ed004aecc0b0e94b3
zman <manzhiyuan@lixiang.com>
2021-03-19 15:19:49 +0800

    feat: 支持 jest 和 junit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/', '/tools/'],
+  // jest-junit is used to generate unit test reports in junit format,
+  // which GitLab can store in Pipeline history.
+  reporters: ['default', 'jest-junit'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  collectCoverage: true,
+}
